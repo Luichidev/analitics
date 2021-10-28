35d5fc9629ef4fe309c3bc7c750889dcb6e8ba0f
@LuisArana <disconluis@gmail.com>
2021-10-28 11:36:32 +0200

    feat(root): Created a file for each index function

--- generar_registro.php
+++ generar_registro.php
@@ -1,3 +1,11 @@
 <?php 
+/**
+ * Author: Luis Alberto Arana M.
+ * twitter: @luichidev
+ * Web: https://luisalbertoarana.com
+ * Creation: 19/10/2021
+ * Revision: 26/10/2021
+ */
+
 include_once("model/Table.php");
 Table::array_to_csv();
--- mostvisited.php
+++ mostvisited.php
@@ -1,4 +1,12 @@
 <?php 
+/**
+ * Author: Luis Alberto Arana M.
+ * twitter: @luichidev
+ * Web: https://luisalbertoarana.com
+ * Creation: 19/10/2021
+ * Revision: 26/10/2021
+ */
+
 include_once("model/Table.php");
 
 function register_mostVisited(){
